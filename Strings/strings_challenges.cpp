2d0405c3ec3bf12f144cdd36245c5b5c291bdd45
shruti248 <shrutiunadkat5@gmail.com>
2023-01-31 10:08:39 +0530

    UpperCase , Lowercase : 2 Methods

---
+++ Strings/strings_challenges.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+
+    public:
+        void stringToUpperCaseAndLowerCase(string str){
+            // cout<<'a'-'A'<<endl; //32 : Means a is 32 ahead of Capital A. 
+
+            // 1) To Uppercase
+            // Therefore every lower case will be stretched back by 32 charaters for ASCII 
+            for(int i = 0 ; i<str.size() ; i++){
+                if(str[i] >='a'  && str[i]<='z'){
+                    str[i]-=32;
+                }
+            }
+
+            cout<<str<<endl;
+
+            // 2) To Lowercase
+            for(int i = 0 ; i<str.size() ; i++){
+                if(str[i] >='A' && str[i]<='Z'){
+                    str[i]+=32;
+                }
+            }
+            cout<<str<<endl;
+
+            // 3) Inbuilt Function 
+            transform(str.begin() , str.end() , str.begin()+3 /**Uppercase will be converted from this character*/, ::toupper);
+            cout<<str<<endl;
+            
+            
+            transform(str.begin() , str.end() , str.begin()+6/**Lowercase will be converted from this character*/, ::tolower);
+            cout<<str<<endl;
+
+
+        }
+
+};
+
+int main(){
+    Solution s;
+    string str;
+
+    cin>>str;
+    s.stringToUpperCaseAndLowerCase(str);
+
+
+    return 0;
+}
