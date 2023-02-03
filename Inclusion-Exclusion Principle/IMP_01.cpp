e477bc1b73fe000936f295df6256ef00c054f2cc
shruti248 <shrutiunadkat5@gmail.com>
2023-02-03 08:43:13 +0530

    Inclusion exclusion Principle

---
+++ Inclusion-Exclusion Principle/IMP_01.cpp
@@ -0,0 +1,9 @@
+// Useful when there are problems where we need to find the number of ways to do something 
+
+// n1 : Way 1 
+// n2 : Way 2 
+// n3 : Both ways 
+
+// Total : n1+n2-n3 (Adding both the ways and subtracting the commom way ..... THis is the inclusion exclusion Principle)
+
+
