59f1d3edd3a207ddff535780c7c376455323e586
shruti248 <shrutiunadkat5@gmail.com>
2023-02-03 10:00:38 +0530

    Recursion 2

--- Recursion/recursion_01.cpp
+++ Recursion/recursion_01.cpp
@@ -1,37 +1,61 @@
 // Function calls Itself to make the problem smaller
 
-// Add All The Numbers till n
-// Sum till n = n+ n-1 + n-2 ...... +1
-// Sum till n =  n+ sum till n-1
-// Sum till n-1 =  n-1 + sum till n-2
-// And so on.....
-
-// Sum till 0 = 0
-// Stop at 0 (Endpoint where it eeds to be stopped).
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-        int Sum(int n){
+class Solution
+{
+public:
+    // Add All The Numbers till n
+    // Sum till n = n+ n-1 + n-2 ...... +1
+    // Sum till n =  n+ sum till n-1
+    // Sum till n-1 =  n-1 + sum till n-2
+    // And so on.....
 
-            // Ending point 
-            if(n == 0){
-                return 0;
-            }
-            int prevSum  = Sum(n-1);
-            // cout<<prevSum<<endl;
-            return n+prevSum;
+    // Sum till 0 = 0
+    // Stop at 0 (Endpoint where it eeds to be stopped).
+    int Sum(int n)
+    {
+
+        // Ending point
+        if (n == 0)
+        {
+            return 0;
         }
+        int prevSum = Sum(n - 1);
+        // cout<<prevSum<<endl;
+        return n + prevSum;
+    }
+
+    // Calculate n raised to power p
+    // n^p = n*n*n*n*.......p times
+    // n^p = n*n^p-1
+    // n^p-1 = n*n^p-2
+    // n^p-2 = n*n^p-3
+    // And So on
+
+    //  n^0 = 1
+    // Stop
+    int powerOfN(int n, int p)
+    {
+        if (p == 0)
+        {
+            return 1;
+        }
+
+        int prevRes = powerOfN(n, p - 1);
+        return n * prevRes;
+    }
 };
 
-int main(){
+int main()
+{
     Solution s;
     int n;
-    cin>>n;
-    cout<<s.Sum(n);
+    cin >> n;
+    // cout<<s.Sum(n);
+    int p;
+    cin >> p;
+    cout << s.powerOfN(n, p);
     return 0;
 }
-
-
