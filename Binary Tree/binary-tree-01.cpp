67ce170883c7828cf99721389228cb3ecc7deae0
shruti248 <shrutiunadkat5@gmail.com>
2023-05-20 17:08:54 +0530

    Level Order Traversal

--- Binary Tree/binary-tree-01.cpp
+++ Binary Tree/binary-tree-01.cpp
@@ -121,18 +121,44 @@ Node* buildTreePostOrderGiven(int postOrder[] , int inOrder[] , int start , int 
     return node;
 }
 
+// LEVEL ORDER TRAversal : QUEUE : FIFO
+void printLevelOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* node = q.front();
+        q.pop();
+        if(node != NULL){
+            cout<<node->data<<" ";
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
+            }
+        }else if(!q.empty()){
+            q.push(NULL);
+        }
+    }
+}
 
 
 int main(){
-    // struct Node* root = new Node(1);
-    // root->left = new Node(2);
-    // root->right = new Node(3);
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
 
-    // root->left->left = new Node(4);
-    // root->left->right = new Node(5);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
 
-    // root->right->left = new Node(6);
-    // root->right->right = new Node(7);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
 
     // preOrder(root);
     // cout<<endl;
@@ -145,11 +171,13 @@ int main(){
     // cout<<endl;
 
     // int preorder[] = {1 ,2  , 4 , 3 , 5};
-    int postorder[] = {4 , 2 , 5 , 3 , 1};
-    int inorder[] = { 4 , 2 , 1 , 5 , 3};
+    // int postorder[] = {4 , 2 , 5 , 3 , 1};
+    // int inorder[] = { 4 , 2 , 1 , 5 , 3};
 
     // Node* root = buildTree(preorder , inorder , 0  , 4);
-    Node* root = buildTreePostOrderGiven(postorder , inorder , 0  , 4);
-    inOrder(root);
+    // Node* root = buildTreePostOrderGiven(postorder , inorder , 0  , 4);
+    // inOrder(root);
+
+    printLevelOrder(root);
     return 0;
 }
