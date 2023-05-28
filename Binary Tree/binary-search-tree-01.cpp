e562af38d858a0e4cfc7d65311e7a3c69674756b
shruti248 <shrutiunadkat5@gmail.com>
2023-05-28 15:40:01 +0530

    Small Change in Last Question

--- Binary Tree/binary-search-tree-01.cpp
+++ Binary Tree/binary-search-tree-01.cpp
@@ -637,11 +637,11 @@ int smallestSubarraywithSum(int arr[] , int n , int x){
     int sum = 0  , minLength = n+1 , start = 0 , end = 0;
 
     while(end < n){
-        while(sum  <= x && end < n){
+        while(sum  < x && end < n){
             sum += arr[end++];
         }
 
-        while(sum > x && start < n){
+        while(sum >= x && start < n){
             if(end - start < minLength){
                 minLength = end - start;
             }
