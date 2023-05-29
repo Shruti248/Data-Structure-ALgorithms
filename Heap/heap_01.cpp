4fea8973161ce9b03c650df29d5ce9ce17fc9f9c
shruti248 <shrutiunadkat5@gmail.com>
2023-05-29 16:50:16 +0530

    Heap STL : Heap Sort , is_heap , is_heap_until

--- Heap/heap_01.cpp
+++ Heap/heap_01.cpp
@@ -108,20 +108,37 @@ void heapSort(vector<int> &arr)
 }
 
 // Heap STL
-void heapUsingSTL(vector<int> heap){
+void heapUsingSTL(vector<int> heap)
+{
     // By default : Max Heap;
-    make_heap(heap.begin() , heap.end());
+    make_heap(heap.begin(), heap.end());
 
     heap.push_back(4);
     heap.push_back(20);
-    push_heap(heap.begin() , heap.end());
+    push_heap(heap.begin(), heap.end());
 
-    pop_heap(heap.begin() , heap.end());
+    pop_heap(heap.begin(), heap.end());
     heap.pop_back();
 
+    // deafult : Ascending Order
+    sort_heap(heap.begin(), heap.end());
+
+    // is maxHeap = true
+    // if not : false
+    bool isHeap = is_heap(heap.begin(), heap.end());
+    cout << isHeap << endl;
+
+    auto iteratortillHeap = is_heap_until(heap.begin(), heap.end());
+    for (auto it = heap.begin(); it != iteratortillHeap; it++)
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+
     // cout<<"Max Element : "<<heap.front()<<endl;
-    for(int i = 0 ; i<heap.size() ; i++){
-        cout<<heap[i]<<" ";
+    for (int i = 0; i < heap.size(); i++)
+    {
+        cout << heap[i] << " ";
     }
 }
 
