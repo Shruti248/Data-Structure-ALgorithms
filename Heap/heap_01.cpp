433464073ee31e72207b088da3bd7922826f2662
shruti248 <shrutiunadkat5@gmail.com>
2023-05-29 15:50:46 +0530

    Heap : Insertion

---
+++ Heap/heap_01.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(vector<int> &heap, int i)
+{
+    int size = heap.size();
+    int largest = i;
+
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && heap[left] > heap[largest])
+        largest = left;
+    if (right < size && heap[right] > heap[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        swap(heap[i], heap[largest]);
+        heapify(heap, largest);
+    }
+}
+
+void insert(vector<int> &heap, int newNum)
+{
+    int size = heap.size();
+
+    if (size == 0)
+    {
+        heap.push_back(newNum);
+    }
+    else
+    {
+        heap.push_back(newNum);
+
+        for (int i = size / 2 - 1; i >= 0; i--)
+        {
+            heapify(heap, i);
+        }
+    }
+}
+
+int main()
+{
+    vector<int> heap = {};
+    insert(heap, 10);
+    insert(heap, 8);
+    insert(heap, 5);
+    insert(heap, 15);
+    insert(heap, 6);
+
+    for (int i = 0; i < heap.size(); i++)
+    {
+        cout << heap[i] << " ";
+    }
+    return 0;
+}
