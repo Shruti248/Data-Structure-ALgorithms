69e4b5eac4358c2c01ac249f2e87855dd5519ca0
shruti248 <shrutiunadkat5@gmail.com>
2023-05-31 19:43:06 +0530

    Greedy : Activity Selection Problem

--- Greedy Algorithm/greedy-01.cpp
+++ Greedy Algorithm/greedy-01.cpp
@@ -21,8 +21,35 @@ int coinChange(vector<int> denominations , int amount){
 
 }
 
+// Activity Selection Problem
+int activitySelectionProblem(vector<vector<int>> v /**Start & end Times*/){
+    int n = v.size();
+
+    // Sort Acc to finish Times
+    sort(v.begin() , v.end() , [&](vector<int> &a , vector<int> &b){
+        return a[1] < b[1];
+    });
+
+    // Taking the first Activity
+    int take = 1;
+    int end = v[0][1];
+    for(int i = 1 ; i<n ; i++){
+        if(v[i][0] >= end){
+            take++;
+            end = v[i][1];
+        }
+    }
+
+    return take;
+
+}
+
 int main(){
-    vector<int> denomiations = {1 , 2 , 5 , 10 , 20 , 50 , 100 , 200 , 500 , 2000};
-    cout<<coinChange(denomiations , 350);
+    // vector<int> denomiations = {1 , 2 , 5 , 10 , 20 , 50 , 100 , 200 , 500 , 2000};
+    // cout<<coinChange(denomiations , 350);
+
+    vector<vector<int>> times = {{1, 2}, {3, 4}, {0, 6}, {5, 7}, {8, 9}, {5, 9}};
+
+    cout<<activitySelectionProblem(times);
     return 0;
 }
