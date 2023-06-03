e7280533eb6662ce7c78aab39546334f7268524b
shruti248 <shrutiunadkat5@gmail.com>
2023-06-04 00:08:35 +0530

    DP : Intro

---
+++ Dynamic Programming/dp-01.cpp
@@ -0,0 +1,25 @@
+//DP
+// Enhanced Recursion : Parent problem is recursion --> All the prob solved by recursion is solved by DP
+        // How to recognize DP :
+
+            // 1) CHOICE : We will be given a choice (like whether ti include a element or not)
+            // RECURSION L 2 calls (overlapping problems) --> means DP can be applied. if recursion calls only 1 function --> NO DP
+            // 2)OPTIMAL : Min , max , largest
+
+            // CHOice & optimal IMPORTANT
+// OPTIMAL
+
+// How to solve :
+// RECURSIVE SOLUTION ---> MEMOIZATION OR TOP DOWN
+
+// PARENT PROBLEMS IN DP (ALL THE OTHER ARE VARIATIONS OF THIS PROBLEMS ONLY)
+// 1) 0-1 KANPSACK (6) (Subset , equal sum partition , count subset)
+// 2) UNBOUNDED KNAPSACK (5)
+// 3) FIBONNACI(7)
+// 4) LCS(15)
+// 5) LIS(10)
+// 6) KADANES ALGO(6)
+// 7) MATRIX CHAIN MULTIPLICATION(7) --> MOST IMP
+// 8) DP ON TREES (4)
+// 9) DP ON GRID(14)
+// 10) OTHERS(5)
