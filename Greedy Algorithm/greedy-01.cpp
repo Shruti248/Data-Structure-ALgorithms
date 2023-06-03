8308bf1e3bb3964688926d1a9fb75b83cc2a897c
shruti248 <shrutiunadkat5@gmail.com>
2023-06-03 18:21:58 +0530

    Greedy : Find Maximum Equal sum of Three Stacks

--- Greedy Algorithm/greedy-01.cpp
+++ Greedy Algorithm/greedy-01.cpp
@@ -253,6 +253,45 @@ int maxLevel(int a[] , int n){
     return ans;
 }
 
+
+// Find Maximum Equal sum of Three Stacks
+// Time Complexity : O(n1 + n2 + n3) where n1, n2 and n3 are sizes of three stacks.
+
+// Auxiliary space: O(1) because using constant space for variables
+
+int maxSum(int stack1[], int stack2[], int stack3[], int n1,
+           int n2, int n3)
+{
+    int sum1 = 0, sum2 = 0, sum3 = 0;
+
+    for (int i = 0; i < n1; i++)
+        sum1 += stack1[i];
+
+    for (int i = 0; i < n2; i++)
+        sum2 += stack2[i];
+
+    for (int i = 0; i < n3; i++)
+        sum3 += stack3[i];
+
+
+    int top1 = 0, top2 = 0, top3 = 0;
+    while (1) {
+        if (top1 == n1 || top2 == n2 || top3 == n3)
+            return 0;
+
+        if (sum1 == sum2 && sum2 == sum3)
+            return sum1;
+
+        if (sum1 >= sum2 && sum1 >= sum3)
+            sum1 -= stack1[top1++];
+        else if (sum2 >= sum1 && sum2 >= sum3)
+            sum2 -= stack2[top2++];
+        else if (sum3 >= sum2 && sum3 >= sum1)
+            sum3 -= stack3[top3++];
+    }
+}
+
+
 int main()
 {
     // vector<int> denomiations = {1 , 2 , 5 , 10 , 20 , 50 , 100 , 200 , 500 , 2000};
@@ -292,9 +331,19 @@ int main()
     // cout<<findMinSum(a , b , 4);
 
 
-    int a[] = {10, 20, 30, 50, 60, 70};
+    // int a[] = {10, 20, 30, 50, 60, 70};
 
-    cout<<maxLevel(a , 6);
+    // cout<<maxLevel(a , 6);
+
+    int stack1[] = { 3, 2, 1, 1, 1 };
+    int stack2[] = { 4, 3, 2 };
+    int stack3[] = { 1, 1, 4, 1 };
+
+    int n1 = sizeof(stack1) / sizeof(stack1[0]);
+    int n2 = sizeof(stack2) / sizeof(stack2[0]);
+    int n3 = sizeof(stack3) / sizeof(stack3[0]);
+
+    cout << maxSum(stack1, stack2, stack3, n1, n2, n3)
 
     return 0;
 }
