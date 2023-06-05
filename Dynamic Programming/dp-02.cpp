4b60b242cc75a1f498250fc0ec4f93c36b7676b9
shruti248 <shrutiunadkat5@gmail.com>
2023-06-05 23:27:44 +0530

    DP : LCS Memorized

--- Dynamic Programming/dp-02.cpp
+++ Dynamic Programming/dp-02.cpp
@@ -27,11 +27,37 @@ int LCS(string x, string y, int xlen, int ylen)
         return max(LCS(x, y, xlen - 1, ylen), LCS(x, y, xlen, ylen - 1));
 }
 
+// Memorized Version (Bottom Up Approach)
+int static t[1001][1001];
+int LCSMemorized(string x, string y, int xlen, int ylen)
+{
+    // Bse condition
+    if (xlen == 0 || ylen == 0)
+    {
+        return 0;
+    }
+
+    // int t[xlen+1][ylen+1]; --> Depends on constraint : If x , y max value is 1000 , take one more .
+
+    if(t[xlen][ylen] != -1)
+        return t[xlen][ylen];
+
+    // Choice Diagram
+    if (x[xlen - 1] == y[ylen - 1])
+        return t[xlen][ylen] = 1 + LCSMemorized(x, y, xlen - 1, ylen - 1);
+    else
+        return t[xlen][ylen] = max(LCSMemorized(x, y, xlen - 1, ylen), LCSMemorized(x, y, xlen, ylen - 1));
+}
+
 
 int main()
 {
     string x = "abcde";
     string y="cbe";
-    cout<<LCS(x , y , 5 , 3);
+    // cout<<LCS(x , y , 5 , 3);
+
+
+    memset(t , -1 , sizeof(t));
+    cout<<LCSMemorized(x , y , 5 , 3);
     return 0;
 }
