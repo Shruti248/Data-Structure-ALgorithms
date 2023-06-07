56cf4bb12ac8c758dd0b6f6c3147b76048d6e6a1
shruti248 <shrutiunadkat5@gmail.com>
2023-06-07 16:41:11 +0530

    DP : Matrix Chain Multiplication Recursive Code

--- Dynamic Programming/dp-03.cpp
+++ Dynamic Programming/dp-03.cpp
@@ -2,12 +2,12 @@
 
 // Identification
 // Given : String or Array
-        // Take some i & j
-        // Take k between k
-        // SO larger problem (i to j) --> divided in various subproblems(i to k & k+1 to j)
-        // SO smaller problems will be solved by taking k everytime between i to j
-        // All the partition of different k will result in some temp ans;
-        // We apply some function in the temp ans to get the final ans
+// Take some i & j
+// Take k between k
+// SO larger problem (i to j) --> divided in various subproblems(i to k & k+1 to j)
+// SO smaller problems will be solved by taking k everytime between i to j
+// All the partition of different k will result in some temp ans;
+// We apply some function in the temp ans to get the final ans
 
 // Main format code : All the questions based on MCM will have this format
 
@@ -36,3 +36,76 @@
 // }
 
 // Change teh format as per the questions
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// MCM Recursive
+// Input : Arr[](A1 A2 A3 ... --> Matrix (Find dimensionfrom that))
+// Output : Multiply the array inputs such that the cost is minimum
+
+// ARRAY SIze : n --->  MATRIX SIZE : n-1
+
+// FORMULA FOR CALCULATING DIMENSION
+// Ai = arr[i-1] * arr[i]
+
+// FORMULA FOR CALCUING COST OF MATRIX
+// Eg : Matrix are : 20 x 3 & 3 x 10
+// COst : 20 x 3(common) x 10 --> 600
+
+// Eg : [40 20 30 10 30]
+// arrsize = 5 then matric is 4
+// Dimension of matrixs
+// A1 --> 40 x 20
+// A2 --> 20 x 30
+// A3 --> 30 x 10
+// A4 --> 10 x 30
+
+// CHOOSING i & j  --> Passed in main Function
+// i --> left end --> index 1 --> bcoz at index 0(Ai = arr[i-1] * arr[i]) is violated
+// j --> right end --> n-1(Last index)
+
+int solve(int arr[], int i, int j)
+{
+
+    // Base Case
+    // i == j --> 1 matrix --> No multiplication for 1 matrix
+    if (i >= j)
+    {
+        return 0;
+    }
+
+    // 2 Possible cases --> do any one
+    // k = i , k = j-1 , i to k , k+1 to j
+    // k = i-1 , k = j , i to k-1 , k to j
+
+    // Eg : i to k (AB)
+    // Eg : k+1 to j (CD)
+    // Total cost : (AB) * (CD) --> Multiplying AB & CD is also a cost
+    // That is multiplying the 2 groups cost.
+
+    // Calculating the cost of 2 groups
+    // arr[i-1]*arr[k]*arr[j];
+
+
+    int ans = INT_MAX;
+    for (int k = i; k <= j - 1; k++)
+    {
+        int cost = arr[i-1]*arr[k]*arr[j];
+        int tempAns = solve(arr, i, k) + solve(arr, k + 1, j) + cost;
+
+        ans = min(ans , tempAns);
+    }
+
+    return ans;
+}
+
+int main()
+{
+    // int arr[] = {40, 20, 30, 10, 30};
+    // cout << solve(arr, 1 /**i*/, 4 /**j*/);
+
+    int arr[] = {1 , 2 , 3 , 4 , 3};
+    cout<<solve(arr , 1 , 4);
+    return 0;
+}
