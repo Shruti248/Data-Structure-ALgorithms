70e23ec367c0a9a31b8d91e68b27236549bfdb2d
shruti248 <shrutiunadkat5@gmail.com>
2023-06-19 08:07:10 +0530

    BS on reverse Sorted array

--- Binary Search/BS-01.cpp
+++ Binary Search/BS-01.cpp
@@ -22,8 +22,32 @@ int search(vector<int> nums , int ele){
     return -1;
 }
 
+// BS on reverse Sorted array
+int searchonReverseSortedArray(vector<int> nums , int ele){
+    int start = 0;
+    int end = nums.size()-1;
+
+    while(start <= end){
+        int mid = (start+end)/2;
+
+        if(nums[mid] == ele){
+            return mid;
+        }else if(nums[mid] < ele){
+            // Left
+            end = mid-1;
+        }else{
+            start = mid+1;
+        }
+    }
+
+    return -1;
+}
+
 int main(){
-    vector<int> nums = {1 , 2 , 3 , 4};
-    cout<<search(nums , 1);
+    // vector<int> nums = {1 , 2 , 3 , 4};
+    // cout<<search(nums , 1);
+
+    vector<int> nums = {4 , 3 , 2 , 1};
+    cout<<searchonReverseSortedArray(nums , 1);
     return 0;
 }
