b890f81ecc4cf679e858dc196e09110cf3b49208
shruti248 <shrutiunadkat5@gmail.com>
2023-06-19 08:57:05 +0530

    Count of the element in a sorted Array

--- Binary Search/BS-01.cpp
+++ Binary Search/BS-01.cpp
@@ -166,6 +166,65 @@ vector<int> firstAndLastOccurenceELe(vector<int> nums, int ele)
 
     return res;
 }
+
+// Count of the element in a sorted Array
+int countSortedArray(vector<int> nums, int ele)
+{
+    int start = 0;
+    int first = -1, last = -1;
+    int end = nums.size() - 1;
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if (ele == nums[mid])
+        {
+            first = mid;
+            end = mid - 1;
+        }
+        else if (nums[mid] > ele)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+
+    start = 0;
+    end = nums.size() - 1;
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if (ele == nums[mid])
+        {
+            last = mid;
+            start = mid + 1;
+        }
+        else if (nums[mid] > ele)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+
+    if(first == -1 && last == -1){
+        return 0;
+    }
+
+    int res = 0;
+    res = last-first+1;
+
+    return res;
+}
+
 int main()
 {
     // vector<int> nums = {1 , 2 , 3 , 4};
@@ -177,12 +236,15 @@ int main()
     // vector<int> nums = {1, 2, 3, 4};
     // cout << searchonOrderNotKnown(nums, 2);
 
-    vector<int> nums = {1, 1, 1, 1};
-    vector<int> ans = firstAndLastOccurenceELe(nums, 1);
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i];
-    }
+    // vector<int> nums = {1, 1, 1, 1};
+    // vector<int> ans = firstAndLastOccurenceELe(nums, 1);
+    // for (int i = 0; i < ans.size(); i++)
+    // {
+    //     cout << ans[i];
+    // }
+
+    vector<int> nums = {1, 1, 1, 10};
+    cout << countSortedArray(nums, 1);
 
     return 0;
 }
