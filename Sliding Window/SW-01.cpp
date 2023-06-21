b19da2ce8d75e9b9bd939df2e235648252f5c14f
shruti248 <shrutiunadkat5@gmail.com>
2023-06-22 02:27:36 +0530

    First negative integer in every window of size k

--- Sliding Window/SW-01.cpp
+++ Sliding Window/SW-01.cpp
@@ -52,9 +52,63 @@ int maxSumSubarray(vector<int> arr, int k)
 
     return mx;
 }
+
+// First negative integer in every window of size k
+vector<int> firstNegativeNumber(vector<int> arr, int k)
+{
+
+    int i = 0, j = 0;
+    list<int> negativeNums;
+    vector<int> ans;
+
+    while (j < arr.size())
+    {
+        // Calculations of j
+        if (arr[j] < 0)
+        {
+            negativeNums.push_back(arr[j]);
+        }
+
+        if (j - i + 1 < k)
+        {
+            j++;
+        }
+        else if (j - i + 1 == k)
+        {
+            if (negativeNums.size() == 0)
+            {
+                // No Negative number
+                ans.push_back(0);
+            }
+            else
+            {
+                ans.push_back(negativeNums.front());
+            }
+            // Slide the window and if negative number removaed while sliding , remove from list as well
+            if (arr[i] < 0)
+            {
+                negativeNums.pop_front();
+            }
+
+            i++;
+            j++;
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
-    vector<int> arr = {1 , 2 , 3 , 4 , 5};
-    cout<<maxSumSubarray(arr , 3);
+    // vector<int> arr = {1 , 2 , 3 , 4 , 5};
+    // cout<<maxSumSubarray(arr , 3);
+
+    vector<int> arr = {1, -2, -3, 4, -5};
+    vector<int> ans = firstNegativeNumber(arr, 3);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
     return 0;
 }
