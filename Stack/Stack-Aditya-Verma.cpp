e6ca3bff23dcdfdd02d369bb612b3eae71163254
shruti248 <shrutiunadkat5@gmail.com>
2023-06-26 00:05:30 +0530

    Stock Span Problem

--- Stack/Stack-Aditya-Verma.cpp
+++ Stack/Stack-Aditya-Verma.cpp
@@ -202,14 +202,83 @@ vector<int> nearestSmallerToRight(vector<int> arr)
     return ans;
 }
 
+// Nearest greater to left : Equivalent Problem : consecutive smaller or equal before it --> Stock Span Problem
+
+// arr : 100 80 60 70 60 75 85
+//First : Find nearest Greater elemenet to the left  for each element
+// 100 80 80 80 70 80 100
+
+// Now suppose you want to find consecutive smaller or equal before it
+// For index : i = 5 (75) --> Nearest greater element ( i = 1) --> 5-1 : 4 --> ANSWER
+
+// Required ans : Substract the indices of the current element with the nearest greater element
+
+
+// We used to store the element in vector : Now we store the indices
+// Indices of nearest greater to left : -1 0 1 1 3 0
+// Output : i - indexFound : Eg (0- -1 : 1) (1 - 0 : 1)...So on
+
+
+// Required : Index and element in stack
+// Use : stack<pair<int , int> st;
+
+vector<int> stockSpan(vector<int> arr)
+{
+    stack<pair<int , int>> st;
+    vector<int> ans;
+
+    for (int i = 0; i < arr.size() ; i++)
+    {
+        if (st.empty())
+        {
+            ans.push_back(-1);
+        }
+        else if (st.size() > 0 && st.top().first > arr[i])
+        {
+            ans.push_back(st.top().second); //in the vector store indexes only
+        }
+        else if (st.size() > 0 && st.top().first<= arr[i])
+        {
+            while (st.size() > 0 && st.top().first <= arr[i])
+            {
+                st.pop();
+            }
+
+            if (st.empty())
+            {
+                ans.push_back(-1);
+            }
+            else
+            {
+                ans.push_back(st.top().second);
+            }
+        }
+
+        st.push({arr[i] , i});
+    }
+
+    for(int i = 0 ; i<ans.size() ; i++){
+        ans[i] = i - ans[i];
+        // cout<<i-ans[i]<<" ";
+    }
+
+    return ans;
+}
+
+
+
+
+
 
 int main()
 {
-    vector<int> arr = {4 , 5 , 2 , 10 , 8};
+    vector<int> arr = {100 , 80 , 60 , 70 , 60 , 75 , 85};
     // vector<int> ans = nextLargestElement(arr);
     // vector<int> ans = nearestGreaterToLeft(arr);
     // vector<int> ans = nearestSmallerToLeft(arr);
-    vector<int> ans = nearestSmallerToRight(arr);
+    // vector<int> ans = nearestSmallerToRight(arr);
+
+    vector<int> ans = stockSpan(arr);
 
 
     for (int i = 0; i < ans.size(); i++)
