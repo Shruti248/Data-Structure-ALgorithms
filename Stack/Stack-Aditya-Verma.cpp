166afdf4572fdbf28a7dae2f1dc318b3b99b7286
shruti248 <shrutiunadkat5@gmail.com>
2023-06-25 20:04:01 +0530

    Nearest Smaller to left

--- Stack/Stack-Aditya-Verma.cpp
+++ Stack/Stack-Aditya-Verma.cpp
@@ -123,11 +123,52 @@ vector<int> nearestGreaterToLeft(vector<int> arr)
     return ans;
 }
 
+// Nearest Smaller to left
+vector<int> nearestSmallerToLeft(vector<int> arr)
+{
+    stack<int> st;
+    vector<int> ans;
+
+    for (int i = 0; i < arr.size() ; i++)
+    {
+        if (st.empty())
+        {
+            ans.push_back(-1);
+        }
+        else if (st.size() > 0 && st.top() < arr[i])
+        {
+            ans.push_back(st.top());
+        }
+        else if (st.size() > 0 && st.top() >= arr[i])
+        {
+            while (st.size() > 0 && st.top() >= arr[i])
+            {
+                st.pop();
+            }
+
+            if (st.empty())
+            {
+                ans.push_back(-1);
+            }
+            else
+            {
+                ans.push_back(st.top());
+            }
+        }
+
+        st.push(arr[i]);
+    }
+
+    return ans;
+}
+
+
 int main()
 {
-    vector<int> arr = {1, 3, 2, 4};
+    vector<int> arr = {4 , 5 , 2 , 10 , 8};
     // vector<int> ans = nextLargestElement(arr);
-    vector<int> ans = nearestGreaterToLeft(arr);
+    // vector<int> ans = nearestGreaterToLeft(arr);
+    vector<int> ans = nearestSmallerToLeft(arr);
 
     for (int i = 0; i < ans.size(); i++)
     {
