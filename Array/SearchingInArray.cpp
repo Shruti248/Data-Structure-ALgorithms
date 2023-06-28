79bbcfd13069b19a574cfbc9a25d88728a2d2b39
shruti248 <shrutiunadkat5@gmail.com>
2023-06-28 19:50:49 +0530

    Min and max element in array using the minimum number of camparisons (O(n) O(1))

--- Array/SearchingInArray.cpp
+++ Array/SearchingInArray.cpp
@@ -1,6 +1,6 @@
 // Linear & Bineary Search
 #include <bits/stdc++.h>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 
 class Solution
@@ -27,10 +27,11 @@ public:
 
     // Works only on the SORTED ARRAY
     int binarySearch(int arr[], int n, int k)
-    {   
-        // if the array is not already sorted , sort it first 
-        if( ! is_sorted(arr , arr+n)){
-            sort(arr , arr+n);
+    {
+        // if the array is not already sorted , sort it first
+        if (!is_sorted(arr, arr + n))
+        {
+            sort(arr, arr + n);
         }
         int low = 0, high = n - 1, mid;
         while (low <= high)
@@ -54,23 +55,95 @@ public:
         return -1;
     }
 };
-int main()
-{
-    Solution s;
-    int n; // size of array
-    cin >> n;
 
-    int arr[n];
-    for (int i = 0; i < n; i++)
+// Min and max element in array using the minimum number of camparisons (O(n) O(1))
+
+void minMaxEle(vector<int> arr)
+{
+    int n = arr.size();
+
+    int mn = INT_MAX;
+    int mx = INT_MIN;
+    // If odd : 0th element is min and max
+    // if even : min nd max as per elements
+
+    // Then process the elements in pair
+
+    int i;
+    if (n % 2 == 0)
     {
-        cin >> arr[i];
+        // Even
+        if (arr[0] > arr[1])
+        {
+            mx = arr[0];
+            mn = arr[1];
+        }
+        else
+        {
+            mx = arr[1];
+            mn = arr[0];
+        }
+        i = 2;
+    }
+    else
+    {
+        // Odd
+        mx = arr[0];
+        mn = arr[0];
+        i = 1;
     }
 
-    int k; // Element to be Searched
-    cin >> k;
+    // Process 2 elements at once
+    while (i < n - 1)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            if (arr[i] > mx)
+            {
+                mx = arr[i];
+            }
+
+            if (arr[i + 1] < mn)
+            {
+                mn = arr[i + 1];
+            }
+        }
+        else
+        {
+            if (arr[i + 1] > mx)
+            {
+                mx = arr[i + 1];
+            }
+
+            if (arr[i] < mn)
+            {
+                mn = arr[i];
+            }
+        }
+
+        i += 2;
+    }
+
+    cout << mn << " " << mx;
+}
+
+int main()
+{
+    // Solution s;
+    // int n; // size of array
+    // cin >> n;
+
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+
+    // int k; // Element to be Searched
+    // cin >> k;
 
     // Call the Function as per required-----
-    s.linearSearch(arr, n, k);
+    // s.linearSearch(arr, n, k);
 
     // int result = s.binarySearch(arr, n, k);
     // if (result == -1)
@@ -81,5 +154,8 @@ int main()
     // {
     //     cout << result << endl;
     // }
+
+    vector<int> arr = {1 , 7 , 3 , 4 , 5};
+    minMaxEle(arr);
     return 0;
 }
