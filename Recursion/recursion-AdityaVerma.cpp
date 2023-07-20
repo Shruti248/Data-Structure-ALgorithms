7ab79c43924b65baa85b3432e77a87e642e843a6
shruti248 <shrutiunadkat5@gmail.com>
2023-07-20 18:35:19 +0530

    Recursion - Intro

---
+++ Recursion/recursion-AdityaVerma.cpp
@@ -0,0 +1,15 @@
+// Recursion
+
+// Make Input Smaller : But Why ?
+        // Input becomes automatically smaller on taking some decisions
+
+// Recursion : Decision Space
+        // IDentification : Whether problem statemenet is recursive or not
+        // 1) CHOICES + DECISION
+// Recursion Tree - SOul of Recursion
+        // IMP : If done this tree , problem Solved
+
+
+// 2 steps to solve any recursive problem
+        // 1) Design a recursive tree
+        // 2) Write Code
