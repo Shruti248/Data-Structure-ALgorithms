06d8364e83e0c0b9bf44811709d527a89700239e
shruti248 <shrutiunadkat5@gmail.com>
2023-07-20 19:57:31 +0530

    fibonacci

--- Recursion/recursion-AdityaVerma.cpp
+++ Recursion/recursion-AdityaVerma.cpp
@@ -61,10 +61,26 @@ void printReverse(int n)
 
 }
 
+int fibonacci(int n){
+    if( n == 0){
+        return 0;
+    }
+
+    if(n == 1 || n == 2){
+        return 1;
+    }
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
 int main()
 {
-    print(5);
-    cout<<endl;
-    printReverse(5);
+    // print(5);
+    // cout<<endl;
+    // printReverse(5);
+
+    for(int i = 0 ; i<2 ; i++){
+        cout<<fibonacci(i)<<endl;
+    }
     return 0;
 }
