c87c739889fdc5bf1eb0b8a1682502fd3f632c6b
shruti248 <shrutiunadkat5@gmail.com>
2023-07-30 10:55:40 +0530

    binaryNumberTrianglePattern

--- Patterns/pattern-01.cpp
+++ Patterns/pattern-01.cpp
@@ -1,18 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Sqaure Pattern
-void square(int n){
-    for(int  i = 0 ; i<n ; i++){
-        for(int j = 0 ; j<n ; j++){
-            cout<<" * ";
+void square(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << " * ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-void rightAngledTriangle(int n){
-    for(int i = 0 ; i<n ; i++){
+void rightAngledTriangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         // for(int j = 0 ; j<n ; j++){
         //     if(i >= j){
         //         cout<<" * ";
@@ -21,99 +26,142 @@ void rightAngledTriangle(int n){
         //     }
         // }
 
-        for(int j = 0 ; j<=i ; j++){
-            cout<<"*";
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-void rightAngledNumberedPyramid(int n){
-    for(int i = 1 ; i<=n ; i++){
-        for(int j = 1 ; j<=i ; j++){
-            cout<<j;
+void rightAngledNumberedPyramid(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-void rightAngledNumberedPyramidII(int n){
-    for(int i = 1 ; i<=n ; i++){
-        for(int j = 1 ; j<=i ; j++){
-            cout<<i;
+void rightAngledNumberedPyramidII(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-void invertedRightPyramid(int n){
-    for(int i = 1 ; i<=n ; i++){
-        for(int j = n ; j>= i ; j--){
-            cout<<"*";
+void invertedRightPyramid(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-void invertedNumberedRightPyramid(int n){
-    for(int i = 1 ; i<=n ; i++){
-        for(int j = n ; j>= i ; j--){
-            cout<<n-j+1;
+void invertedNumberedRightPyramid(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout << n - j + 1;
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-void starPyramid(int n){
-    for(int i = 1 ; i<=n ; i++){
+void starPyramid(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
 
-            // Spaces
-            for(int j = 1 ; j <= n-i ; j++){
-                cout<<" ";
-            }
+        // Spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
 
-            for(int j = 1 ; j<= 2*i-1 ; j++){
-                cout<<"*";
-            }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
 
-            for(int j = 1 ; j <= n-i ; j++){
-                cout<<" ";
-            }
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
 
-            cout<<endl;
+        cout << endl;
     }
 }
 
-void invertedstarPyramid(int n){
-    for(int i = 1 ; i<=n ; i++){
+void invertedstarPyramid(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
 
-            // Spaces
-            for(int j = 1 ; j < i ; j++){
-                cout<<" ";
-            }
+        // Spaces
+        for (int j = 1; j < i; j++)
+        {
+            cout << " ";
+        }
 
-            for(int j = 1 ; j<= 2*n - (2*i-1) ; j++){
-                cout<<"*";
-            }
+        for (int j = 1; j <= 2 * n - (2 * i - 1); j++)
+        {
+            cout << "*";
+        }
 
-            for(int j = 1 ; j < i ; j++){
-                cout<<" ";
-            }
+        for (int j = 1; j < i; j++)
+        {
+            cout << " ";
+        }
 
-            cout<<endl;
+        cout << endl;
     }
 }
 
-void diamondStarPattern(int n){
+void diamondStarPattern(int n)
+{
 
     starPyramid(n);
     invertedstarPyramid(n);
 }
 
-void halfDiamondStarPattern(int n){
-    rightAngledTriangle(n-1);
+void halfDiamondStarPattern(int n)
+{
+    rightAngledTriangle(n - 1);
     invertedRightPyramid(n);
 }
 
-int main(){
+void binaryNumberTrianglePattern(int n)
+{
+
+    int flag;
+    for (int i = 1; i <= n; i++)
+    {
+
+        i%2 == 0 ? flag = 0 : flag = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<flag;
+            flag == 1 ? flag = 0 : flag = 1;
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
     // square(3);
     // rightAngledTriangle(6);
     // rightAngledNumberedPyramid(5);
@@ -123,6 +171,7 @@ int main(){
     // starPyramid(5);
     // invertedstarPyramid(5);
     // diamondStarPattern(3);
-    halfDiamondStarPattern(5);
+    // halfDiamondStarPattern(5);
+    binaryNumberTrianglePattern(3);
     return 0;
 }
