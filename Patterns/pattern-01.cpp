35afd4b648e4fe1e811510e5e2b2f1bb9bdffb07
shruti248 <shrutiunadkat5@gmail.com>
2023-07-30 10:17:19 +0530

    PATTERNS :  square     rightAngledTriangle     rightAngledNumberedPyramid     rightAngledNumberedPyramidII     invertedRightPyramid     invertedNumberedRightPyramid

---
+++ Patterns/pattern-01.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Sqaure Pattern
+void square(int n){
+    for(int  i = 0 ; i<n ; i++){
+        for(int j = 0 ; j<n ; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
+
+void rightAngledTriangle(int n){
+    for(int i = 0 ; i<n ; i++){
+        // for(int j = 0 ; j<n ; j++){
+        //     if(i >= j){
+        //         cout<<" * ";
+        //     }else{
+        //         cout<<"   ";
+        //     }
+        // }
+
+        for(int j = 0 ; j<=i ; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
+
+void rightAngledNumberedPyramid(int n){
+    for(int i = 1 ; i<=n ; i++){
+        for(int j = 1 ; j<=i ; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+void rightAngledNumberedPyramidII(int n){
+    for(int i = 1 ; i<=n ; i++){
+        for(int j = 1 ; j<=i ; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+void invertedRightPyramid(int n){
+    for(int i = 1 ; i<=n ; i++){
+        for(int j = n ; j>= i ; j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void invertedNumberedRightPyramid(int n){
+    for(int i = 1 ; i<=n ; i++){
+        for(int j = n ; j>= i ; j--){
+            cout<<n-j+1;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    // square(3);
+    // rightAngledTriangle(6);
+    // rightAngledNumberedPyramid(5);
+    // rightAngledNumberedPyramidII(7);
+    // invertedRightPyramid(5);
+    // invertedNumberedRightPyramid(6);
+    return 0;
+}
