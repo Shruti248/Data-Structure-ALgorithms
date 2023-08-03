4ceeb281373e14c7c8ab83ea2b6698fc7ced6090
shruti248 <shrutiunadkat5@gmail.com>
2023-08-03 10:54:30 +0530

    Find Second Smallest and Second Largest Element in an array

--- Hashing/prac.cpp
+++ Hashing/prac.cpp
@@ -126,7 +126,68 @@ bool isSubsetII(vector<int> arr1 , vector<int> arr2){
     return true;
 }
 
-//
+//Find Second Smallest and Second Largest Element in an array
+// 1)with sorting
+// 2) without sorting : 2 traversals
+    // Time Complexity: O(N), We do two linear traversals in our array
+    // Space Complexity: O(1)
+// 3) Using Single Traversal
+
+void secondSmallestAndLargest(vector<int> arr){
+    int smallest = INT_MAX;
+    int largest = INT_MIN;
+
+    int secondSmallest = INT_MAX;
+    int secondLargest = INT_MIN;
+
+    for(int i = 0 ; i<arr.size() ; i++){
+        if(arr[i] < smallest){
+            smallest = arr[i];
+        }
+
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+
+    for(int i = 0 ; i<arr.size() ; i++){
+        if(arr[i] < largest && arr[i] > secondLargest){
+            secondLargest = arr[i];
+        }
+
+        if(arr[i] > smallest && arr[i] < secondSmallest){
+            secondSmallest = arr[i];
+        }
+    }
+
+    if(secondLargest == INT_MIN){
+        secondLargest = -1;
+    }
+
+    if(secondSmallest == INT_MAX){
+        secondSmallest = -1;
+    }
+
+    cout<<secondSmallest<<secondLargest<<endl;
+}
+
+
+// O(N) : Single traversal
+// Similarly seocnd Smallest
+void secondLargestEle(vector<int> arr){
+    int largest = INT_MIN , secondLargest = INT_MIN;
+
+    for(int i = 0 ; i<arr.size() ; i++){
+        if(arr[i] > largest){
+            secondLargest = largest;
+            largest = arr[i];
+        }else if(arr[i] > secondLargest && arr[i] != largest){
+            secondLargest = arr[i];
+        }
+    }
+
+    cout<<secondLargest<<endl;
+}
 
 int main()
 {
@@ -135,6 +196,9 @@ int main()
     // freqHashing({2 , 2 , 4 , 2 , 6 , 6 , 6});
 
     // cout << issubset({11, 1, 13, 21, 3, 7}, {11, 1, 3});
-    cout << isSubsetII({11, 1, 13, 21, 3, 7}, {11, 16, 3});
+    // cout << isSubsetII({11, 1, 13, 21, 3, 7}, {11, 16, 3});
+
+    // secondSmallestAndLargest({1});
+    secondLargestEle({1 , 4 , 2 , 6, 7});
     return 0;
 }
