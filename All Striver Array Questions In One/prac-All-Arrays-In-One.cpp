3c9fa5aad7aa8405e36548db12410124dd97fdb8
shruti248 <shrutiunadkat5@gmail.com>
2023-08-17 09:39:46 +0530

    Heap Sort

--- All Striver Array Questions In One/prac-All-Arrays-In-One.cpp
+++ All Striver Array Questions In One/prac-All-Arrays-In-One.cpp
@@ -2723,6 +2723,41 @@ int kthEle(vector<int> arr1, vector<int> arr2, int k)
     return -1;
 }
 
+
+// Heap Sort
+void heapify(vector<int> &arr , int n , int i){
+    int largest = i;
+
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if(left < n && arr[left] < arr[largest]){
+        largest = left;
+    }
+
+    if(right < n && arr[right] > arr[largest]){
+        largest = right;
+    }
+
+    if(largest != i){
+        swap(arr[largest] , arr[i]);
+        heapify(arr , n , largest);
+    }
+}
+
+void heapSort(vector<int> &arr){
+    int n = arr.size();
+
+    for(int i = n/2-1 ; i>=0 ; i--){
+        heapify(arr , n ,i);
+    }
+
+    for(int i = n-1 ; i>=0 ; i--){
+        swap(arr[0] , arr[i]);
+        heapify(arr , i , 0);
+    }
+}
+
 int main()
 {
     // freq({2 , 2 , 4 , 2 , 6 , 6 });
@@ -2828,11 +2863,14 @@ int main()
     // cout<<allocateBooks({12, 34, 67, 90} , 2);
     // cout<<splitArray({1,2,3,4,5} , 3);
     // cout << medianOf2SortedArrays({2, 4, 6}, {1, 3, 5});
-    cout<<kthEle({2,3,6,7,9} , {1,4,8,10} , 5);
+    // cout<<kthEle({2,3,6,7,9} , {1,4,8,10} , 5);
+    vector<int> ans = {3 , 5 , 2 , 6 , 8};
 
-    // for (int i = 0; i < ans.size(); i++)
-    // {
-    //     cout << ans[i] << " ";
-    // }
+    heapSort(ans);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
     return 0;
 }
